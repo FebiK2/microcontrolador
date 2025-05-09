ce180d6c16f3eb5c783ecc18005c652e28da8d2a
David Riaño <161149533+FebiK2@users.noreply.github.com>
2025-05-08 19:12:57 -0500

    Update Mux_8a1_Main.vhd

--- Mux_8a1/Mux_8a1.srcs/sources_1/new/Mux_8a1_Main.vhd
+++ Mux_8a1/Mux_8a1.srcs/sources_1/new/Mux_8a1_Main.vhd
@@ -3,16 +3,16 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity MUX8to1 is
     Port (
-        A : in STD_LOGIC_VECTOR(7 downto 0);  -- Entrada 0
-        B : in STD_LOGIC_VECTOR(7 downto 0);  -- Entrada 1
-        C : in STD_LOGIC_VECTOR(7 downto 0);  -- Entrada 2
-        D : in STD_LOGIC_VECTOR(7 downto 0);  -- Entrada 3
-        E : in STD_LOGIC_VECTOR(7 downto 0);  -- Entrada 4
-        F : in STD_LOGIC_VECTOR(7 downto 0);  -- Entrada 5
-        G : in STD_LOGIC_VECTOR(7 downto 0);  -- Entrada 6
-        H : in STD_LOGIC_VECTOR(7 downto 0);  -- Entrada 7
+        A : in STD_LOGIC;  -- Entrada 0
+        B : in STD_LOGIC;  -- Entrada 1
+        C : in STD_LOGIC;  -- Entrada 2
+        D : in STD_LOGIC;  -- Entrada 3
+        E : in STD_LOGIC;  -- Entrada 4
+        F : in STD_LOGIC;  -- Entrada 5
+        G : in STD_LOGIC;  -- Entrada 6
+        H : in STD_LOGIC;  -- Entrada 7
         Sel : in STD_LOGIC_VECTOR(2 downto 0); -- Selector
-        Y : out STD_LOGIC_VECTOR(7 downto 0)   -- Salida
+        Y : out STD_LOGIC   -- Salida
     );
 end MUX8to1;
 
